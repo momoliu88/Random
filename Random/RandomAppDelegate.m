e4b7c713c985edfb35735c0f7968aef98dd1a277
liuxiaoqin <momoliu88@126.com>
2013-06-07 22:08:54 +0800

    Initial Commit

---
+++ Random/RandomAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  RandomAppDelegate.m
+//  Random
+//
+//  Created by xiaoqinliu on 13-6-7.
+//  Copyright 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import "RandomAppDelegate.h"
+
+@implementation RandomAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
